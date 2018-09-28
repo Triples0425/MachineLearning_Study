757d6b5cca6f1f754e9b59d1f8e4995ec43364cb
Triples0425 <triples0425@gmail.com>
2018-09-28 21:19:21 +0900

    20180928 - Python 학습

---
+++ Tutorial_ML_DL/AI_with_Python_FollowingBook.py
@@ -0,0 +1,9 @@
+#------------ chapter 1. ---------------------
+#from sklearn import datasets
+
+#house_prices = datasets.load_boston()
+
+#print(house_prices.data)
+
+#print(house_prices.target)
+#------------ chapter 1. end ----------------
