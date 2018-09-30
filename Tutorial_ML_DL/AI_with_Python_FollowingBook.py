fca301c9691b3c11d8e22be7f4dad2656b869fb0
SeungSeob Shin <triples0425@gmail.com>
2018-10-01 00:36:06 +0900

    20181001 - naive

--- Tutorial_ML_DL/AI_with_Python_FollowingBook.py
+++ Tutorial_ML_DL/AI_with_Python_FollowingBook.py
@@ -7,3 +7,22 @@
 
 #print(house_prices.target)
 #------------ chapter 1. end ----------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import cv
+from sklearn.naive_bayes import GaussianNB
+
+
+from utilities import visualize_classifier
+
+input_file = 'data_multivar_nb.txt'
+
+data = np.loadtxt(input_file, delimiter=' , ')
+x, y = data[:, :-1], data[:, -1]
+
+classifier = GaussianNB()
+
+classifier.fit(x, y)
+
