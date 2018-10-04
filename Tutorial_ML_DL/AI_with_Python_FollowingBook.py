d6e156b478831a5be54eb9364f32b3558686b48b
Triples0425 <triples0425@gmail.com>
2018-10-04 18:42:49 +0900

    20181004 - 2번째 스터디

--- Tutorial_ML_DL/AI_with_Python_FollowingBook.py
+++ Tutorial_ML_DL/AI_with_Python_FollowingBook.py
@@ -8,21 +8,27 @@
 #print(house_prices.target)
 #------------ chapter 1. end ----------------
 
+#---------------logisticRegression----------------
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#from sklearn import linear_model
+
+#from utilities import visualize_classifier
+
+#X = np.array([[3.1, 7.2], [4, 6.7], [2.9, 8], [5.1,4.5], [6, 5], [5.6, 5], [3.3, 0.4], [3.9, 0.9], [2.8, 1], [0.5, 3.4], [1, 4], [0.6, 4.9]])
+#y = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3])
+
+#classifier = linear_model.LogisticRegression(solver='liblinear', C=100)
+
+#classifier.fit(X, y)
+
+#visualize_classifier(classifier, X, y)
+#---------------logisticRegression----------------
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn import cv
 from sklearn.naive_bayes import GaussianNB
-
-
-from utilities import visualize_classifier
-
-input_file = 'data_multivar_nb.txt'
-
-data = np.loadtxt(input_file, delimiter=' , ')
-x, y = data[:, :-1], data[:, -1]
-
-classifier = GaussianNB()
-
-classifier.fit(x, y)
+from sklearn.model_selection.cross_val_score import cross_validation
 
