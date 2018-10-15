26195c1fe0a1af74c65b61d6567885fbf0fdb6e9
SeungSeob Shin <triples0425@gmail.com>
2018-10-15 22:31:38 +0900

    20181015 - home_study

--- Tutorial_ML_DL/MyCustom.py
+++ Tutorial_ML_DL/MyCustom.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
 from PIL import Image
+from OptionalFile.mnist import load_mnist
 
 def AND(x1, x2):
     
@@ -99,3 +100,29 @@ def softmax(a):
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, flatten = True, one_hot_label = False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+
+    a3 = np.dot(z2, W3) + b3
+
+    y = softmax(a3)
+
+    return y
